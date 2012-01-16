953f3adce3559a00a554161bb02f81b5e28c2448
Divyendu Singh <divyendu@divyendu-HP-Mini-210-1000.(none)>
2012-01-17 01:09:08 +0530

    testing

--- index.php
+++ index.php
@@ -81,6 +81,5 @@
     <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
   <![endif]-->
-
 </body>
 </html>
